9226be1ca074743b60a2dc5241da4911a8c50b18
deantosh <deantoshdaiddoh@gmail.com>
2023-03-17 08:31:54 -0700

    task 60: script updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,7 +18,6 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", sizeof(a));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(b));
-	printf("Size of a float: %lu byte(s)\n", sizeof(d));
+	printf("Size of a float: %lu byte(s)", sizeof(d));
 	return (0);
 }
-  
