27b1d0640f904cda8f87920e603f976467231110
deantosh <deantoshdaiddoh@gmail.com>
2023-03-17 08:28:04 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/*declare variables*/
+	int a;
+	long int b;
+	char c;
+	float d;
+
+	/*print the size of the data types*/
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(b));
+	printf("Size of a float: %lu byte(s)\n", sizeof(d));
+	return (0);
+}
+  
