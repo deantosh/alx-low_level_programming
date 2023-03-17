a5b1c02b9c89fbe2f70d82020da3106364e07370
deantosh <deantoshdaiddoh@gmail.com>
2023-03-17 01:20:50 -0700

    task 4: script errors updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  *
- * return: 0
+ * Return: Always 0 (succes)
  */
+
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
