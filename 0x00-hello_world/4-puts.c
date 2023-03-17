785a27e7967e0a375e3401134fc6c5eb0aa36fab
deantosh <deantoshdaiddoh@gmail.com>
2023-03-17 00:56:32 -0700

    task 4: script updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-/*include header files*/
 #include <stdio.h>
 
-/*main - Entry point*/
+/*
+ * main - Entry point
+ *
+ * return: 0
+ */
 int main(void)
-
 {
-  /*write string to stdout*/
   puts("\"Programming is like building a multilingual puzzle");
-
-  /*Return: Always 0 (Success)*/
-  return 0;
+  return (0);
 }
