b2e2fd77955a1697c92e0a643ed5e2a6cd2cddba
deantosh <deantoshdaiddoh@gmail.com>
2023-03-21 10:55:26 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - display alphabets
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	/*declare variable*/
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	
+	_putchar('\n');
+	return;
+}
