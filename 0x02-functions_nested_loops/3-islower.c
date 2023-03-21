5c3e8c62aabe08f636b7d75de4c90353eb4a930c
deantosh <deantoshdaiddoh@gmail.com>
2023-03-21 14:12:57 -0700

    _islower prototype added

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,14 @@
 
 /**
  * _islower - checks for lowercase character
+ * @ch: The character to be checked
  *
  * Return: 1 (lowecase) and 0 (not lowercase)
  */
 
 int _islower(int ch)
 {
+	/*use the equivalent ASCII for letters a-z*/
 	if (ch >= 97 && ch <= 122)
 	{
 		return (1);
