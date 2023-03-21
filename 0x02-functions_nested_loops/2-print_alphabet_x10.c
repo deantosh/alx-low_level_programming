16c2c3fa425a3947a2f6aaec3f2ef6e588da9e23
deantosh <deantoshdaiddoh@gmail.com>
2023-03-21 11:44:42 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - creates alphabets 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	/*declare variables*/
+	int i, ch;
+
+	for (i = 0; i <= 10; i++)
+	{
+		/*call user defined function to print alphabets*/
+		print_alphabet(void);
+	}
+	_putchar('\n');
+}
