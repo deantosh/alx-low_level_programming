017fe91e8b5dc1b990431fdceaa5332223089418
deantosh <deantoshdaiddoh@gmail.com>
2023-03-22 14:16:27 -0700

    task 11: updated

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,20 +10,32 @@
 void print_to_98(int n)
 {
 	/*declare variable*/
-	int i;
+	int i, j, k, l;
 
 	if (n <= 98)
 	{
+		/*compute for the 2 digit*/
+		j = n / 10;
+		k = n % 10;
+		
 		for (i = n; i <= 98; i++)
 		{
-			_putchar(n + '0');
+			_putchar(j + '0');
+			_putchar(i + '0');
 		}
 	}
 	else
 	{
+		/*compute the digits on the three number*/
+		j = n / 100;
+		k = n / 10;
+		l = n % 10;
+		
 		for (i = n; i > 98; i--)
 		{
-			_putchar(n + '0');
+			_putchar(j + '0');
+			_putchar(k + '0');
+			_putchar(l + '0');
 		}
 	}
 	_putchar('\n');
