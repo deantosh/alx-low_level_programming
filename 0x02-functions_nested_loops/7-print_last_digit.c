09b46e076fdf46e98367fd447bbf05e2448024df
deantosh <deantoshdaiddoh@gmail.com>
2023-03-22 02:36:25 -0700

    task 7: output errors resolved

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -15,6 +15,11 @@ int print_last_digit(int num)
 	/*get the remainder of num*/
 	last_digit = num % 10;
 
+	if (last_digit < 0)
+	{
+		last_digit = last_digit * -1;
+	}
+
 	_putchar(last_digit +  '0');
-	return (0);
-}
+	return (last_digit);
+ }
