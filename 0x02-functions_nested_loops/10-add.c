682524a62748c70f847b2b34d9832ff5560527d1
deantosh <deantoshdaiddoh@gmail.com>
2023-03-22 07:35:18 -0700

    task 10: script updated

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,19 +2,18 @@
 
 /**
  * add - adds two integers
- * @num1, @num2: The  numbers to be computed
- * 
+ * @num1: The number to be added
+ * @num2: The number to be added
+ *
  * Return: Always 0 (Success)
  */
 
 int add(int num1, int num2)
 {
-  	/*declare variable*/
-  	int results;
+	/*declare variable*/
+	int results;
 
 	results = num1 + num2;
 
-	_putchar(results + '0');
-
-	return (0);
+	return (results);
 }
