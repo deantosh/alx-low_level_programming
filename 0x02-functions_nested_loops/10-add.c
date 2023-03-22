c92af414970782ddb9a08dbaae65ad82af756dc0
deantosh <deantoshdaiddoh@gmail.com>
2023-03-22 07:29:14 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @num1, @num2: The  numbers to be computed
+ * 
+ * Return: Always 0 (Success)
+ */
+
+int add(int num1, int num2)
+{
+  	/*declare variable*/
+  	int results;
+
+	results = num1 + num2;
+
+	_putchar(results + '0');
+
+	return (0);
+}
