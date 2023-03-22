29a00564da569614cc49e2113dbf29dd518bf6c1
deantosh <deantoshdaiddoh@gmail.com>
2023-03-22 06:57:33 -0700

    task 9: output results error resolved

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,21 +9,31 @@
 void times_table(void)
 {
 	/*declare varriables*/
-	int a, b;
+	int a, b, c, d, e;
 
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			/*declare a variable*/
-			int results;
-
 			/*compute*/
-			results = a * b;
+			c = a * b;
 
-			_putchar(results + '0');
-			_putchar(',');
-			_putchar(' ');
+			if (c > 9)
+			{
+				d = c % 10;
+				e = c / 10;
+
+				_putchar(e + '0');
+				_putchar(d + '0');
+				_putchar(44);
+				_putchar(32);
+			}
+			else
+			{
+				_putchar(c + '0');
+				_putchar(44);
+				_putchar(32);
+			}
 		}
 		_putchar('\n');
 	}
