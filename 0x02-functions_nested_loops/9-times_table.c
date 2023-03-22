5c74029139793aa74a9989c02398d0502a19dcc5
deantosh <deantoshdaiddoh@gmail.com>
2023-03-22 06:26:13 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	/*declare varriables*/
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			/*declare a variable*/
+			int = results;
+
+			/*compute*/
+			results = a * b
+  
+			_putchar(results + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		putchar('\n');
+	}
+  
+}
