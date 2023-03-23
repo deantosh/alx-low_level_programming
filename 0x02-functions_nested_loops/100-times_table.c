5969648a331de95a1fbe805339e605318b2d1ff7
deantosh <deantoshdaiddoh@gmail.com>
2023-03-23 10:27:36 -0700

    task 12: output results errors resolved

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,4 +9,42 @@
 
 void print_times_table(int n)
 {
+	/*declare variables*/
+	int a, b, c, d, e;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (a = 0; a <= 9; a++)
+		{
+			for (b = 0; b <= 9; b++)
+			{
+				/*compute*/
+				c = a * b;
+
+				if (c > 9)
+				{
+					d = c % 10;
+					e = c / 10;
+
+					_putchar(44);
+					_putchar(32);
+					_putchar(e + '0');
+					_putchar(d + '0');
+				}
+				else
+				{
+					if (b != 0)
+					{
+						_putchar(44);
+						_putchar(32);
+						_putchar(32);
+					}
+
+					_putchar(c + '0');
+				}
+			}
+			_putchar('\n');
+		}
+
+	}
 }
