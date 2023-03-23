b77f4199538fffce34992619aace1a9e47a18b9b
deantosh <deantoshdaiddoh@gmail.com>
2023-03-23 08:58:40 -0700

    data types modified for the variables (unsigned long)

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,16 +9,17 @@
 int main(void)
 {
 	/*declare variable*/
-	int count, sum;
-	int fib1 = 0;
-	int fib2 = 1;
+	int count;
+	unsigned long sum;
+	unsigned long fib1 = 0;
+	unsigned long fib2 = 1;
 
 	for (count = 0; count <= 50; count++)
 	{
 		/*add the first no. to the second number*/
 		sum = fib1 + fib2;
 
-		printf("%d", sum);
+		printf("%lu", sum);
 
 		/*set values of variables for the next count*/
 		fib1 = fib2;
