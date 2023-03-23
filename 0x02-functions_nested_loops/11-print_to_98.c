1b3f78bac717027de5a3677deee8088e267698dc
deantosh <deantoshdaiddoh@gmail.com>
2023-03-23 00:25:31 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,9 +17,11 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			if (i < 10)
+			{
 				_putchar(i + '0');
-
+			}
 			else
+			{
 				/*compute for the 2 digit*/
 				j = i / 10;
 				k = i % 10;
@@ -29,7 +31,8 @@ void print_to_98(int n)
 
 				if (i == 98)
 					break;
-
+			}
+			
 			_putchar(',');
 			_putchar(' ');
 		}
@@ -39,13 +42,16 @@ void print_to_98(int n)
 		for (i = n; i => 98; i--)
 		{
 			if (i <= 99)
+			{
 				j = i / 10;
 				k = i % 10;
 
 				_putchar(j + '0');
 				_putchar(k + '0');
+			}
 
 			else
+			{
 				/*compute the digits on the three number*/
 				j = i / 100;
 				k = (i - (j*100)) / 10;
@@ -54,7 +60,7 @@ void print_to_98(int n)
 				_putchar(j + '0');
 				_putchar(k + '0');
 				_putchar(l + '0');
-
+			}
 			_putchar(',');
 			_putchar(' ');
 		}
