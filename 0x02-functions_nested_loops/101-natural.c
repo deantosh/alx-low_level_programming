9e48cf2785ca68536707932e74be9dc07737705f
deantosh <deantoshdaiddoh@gmail.com>
2023-03-23 03:57:51 -0700

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/*declare variables*/
+	int num, r1, r2, sum;
+
+	for (num = 0; num < 1024; num++)
+	{
+		r1 = num % 3;
+
+		if (r1 == 0)
+		{
+			sum += num;
+		}
+
+		r2 = num % 5;
+		
+		if (r2 == 0)
+		{
+			sum += num;
+		}
+	}
+	printf("%d\n", sum)
+}
