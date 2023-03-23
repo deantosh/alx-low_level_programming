efd3b7a883c77a226dd3eedb1b5d494a19167119
deantosh <deantoshdaiddoh@gmail.com>
2023-03-23 00:56:22 -0700

    task 11 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,69 +1,28 @@
 #include "main.h"
 
 /**
- *print_to_98 - prints all natural numbers from n to 98
- *@n: The starting number
+ * print_to_98 - prints all natural numbers from n to 98
+ * @num: The starting number
  *
  *Return: void
  */
 
-void print_to_98(int n)
+void print_to_98(int num)
 {
-	/*declare variable*/
-	int i, j, k, l;
-
-	if (n <= 98)
+	if (num >= 98)
 	{
-		for (i = n; i <= 98; i++)
+		while (num > 98)
 		{
-			if (i < 10)
-			{
-				_putchar(i + '0');
-			}
-			else
-			{
-				/*compute for the 2 digit*/
-				j = i / 10;
-				k = i % 10;
-
-				_putchar(j + '0');
-				_putchar(k + '0');
-
-				if (i == 98)
-					break;
-			}
-			
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ", num--);
 		}
+		printf("%d\n", num)
 	}
 	else
 	{
-		for (i = n; i > 97; i--)
+		while (num < 98)
 		{
-			if (i <= 99)
-			{
-				j = i / 10;
-				k = i % 10;
-
-				_putchar(j + '0');
-				_putchar(k + '0');
-			}
-
-			else
-			{
-				/*compute the digits on the three number*/
-				j = i / 100;
-				k = (i - (j*100)) / 10;
-				l = i % 10;
-
-				_putchar(j + '0');
-				_putchar(k + '0');
-				_putchar(l + '0');
-			}
-			_putchar(',');
-			_putchar(' ');
+		  printf("%d, ", num++);
 		}
+		printf("%d\n", num);
 	}
-	_putchar('\n');
 }
