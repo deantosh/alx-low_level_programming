42112aebb7e37fbf29e603278488adfcf3c95f71
deantosh <deantoshdaiddoh@gmail.com>
2023-03-23 09:48:40 -0700

    task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of evenly-valued fib-sequence not exceeding 4,000,000
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/*declare variables*/
+	unsigned long fib1 = 0;
+	unsigned long fib2 = 1;
+	unsigned long sum, fibsum;
+
+	while (1)
+	{
+		fibsum = fib1 + fib2;
+
+		if (fibsum > 4000000)
+			break;
+
+		/*check if fibsum is even*/
+		if ((fibsum % 2) == 0)
+			sum += fibsum;
+
+		/*set valuess for the next iteration*/
+		fib1 = fib2;
+		fib2 = fibsum;
+	}
+	printf("%lu\n", sum);
+
+	return (0);
+}
