04e2d2d5418a8cc2bd356d99fdc73dbaf6f1e3a0
deantosh <deantoshdaiddoh@gmail.com>
2023-03-23 11:07:48 -0700

    task 12: script updated

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,38 +10,38 @@
 void print_times_table(int n)
 {
 	/*declare variables*/
-	int a, b, c, d, e;
+	int a, b, c;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (a = 0; a <= 9; a++)
+		for (a = 0; a <= n; a++)
 		{
-			for (b = 0; b <= 9; b++)
+			_putchar('0');
+
+			for (b = 1; b <= n; b++)
 			{
+				_putchar(',');
+				_putchar(' ');
+			    
 				/*compute*/
 				c = a * b;
 
-				if (c > 9)
-				{
-					d = c % 10;
-					e = c / 10;
+				if (c <= 9)
+					_putchar(' ');
 
-					_putchar(44);
-					_putchar(32);
-					_putchar(e + '0');
-					_putchar(d + '0');
-				}
-				else
-				{
-					if (b != 0)
-					{
-						_putchar(44);
-						_putchar(32);
-						_putchar(32);
-					}
+				if (c <= 99)
+					_putchar(' ');
 
-					_putchar(c + '0');
+				if (c >= 100)
+				{
+					_putchar((c / 100) + '0');
+					_putchar(((c /10) % 10) + '0');
 				}
+				else if (c <= 99 && c >= 10)
+				{
+					_putchar((c / 10) + '0'); 
+				}
+				_putchar((c % 10) + '0')
 			}
 			_putchar('\n');
 		}
