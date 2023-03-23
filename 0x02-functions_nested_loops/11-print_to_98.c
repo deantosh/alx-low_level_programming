23044a44c233cf2faa754077beae5fc2ad059418
deantosh <deantoshdaiddoh@gmail.com>
2023-03-23 00:08:35 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,15 +16,19 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			/*compute for the 2 digit*/
-			j = i / 10;
-			k = i % 10;
+			if (i < 10)
+				_putchar(i + '0');
 
-			_putchar(j + '0');
-			_putchar(k + '0');
+			else
+				/*compute for the 2 digit*/
+				j = i / 10;
+				k = i % 10;
 
-			if (i == 98)
-				break;
+				_putchar(j + '0');
+				_putchar(k + '0');
+
+				if (i == 98)
+					break;
 
 			_putchar(',');
 			_putchar(' ');
@@ -32,16 +36,27 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (i = n; i > 98; i--)
+		for (i = n; i => 98; i--)
 		{
-			/*compute the digits on the three number*/
-			j = i / 100;
-			k = i / 10;
-			l = i % 10;
+			if (i <= 99)
+				j = i / 10;
+				k = i % 10;
 
-			_putchar(j + '0');
-			_putchar(k + '0');
-			_putchar(l + '0');
+				_putchar(j + '0');
+				_putchar(k + '0');
+
+			else
+				/*compute the digits on the three number*/
+				j = i / 100;
+				k = (i - (j*100)) / 10;
+				l = i % 10;
+
+				_putchar(j + '0');
+				_putchar(k + '0');
+				_putchar(l + '0');
+
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
 	_putchar('\n');
