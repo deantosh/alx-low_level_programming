cb08eed8da9955d13cc6b1b3c2108089e33885bb
deantosh <deantoshdaiddoh@gmail.com>
2023-03-23 08:47:10 -0700

    task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry pointt
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/*declare variable*/
+	int count, sum;
+	int fib1 = 0;
+	int fib2 = 1;
+
+	for (count = 0; count <= 50; i++)
+	{
+		/*add the first no. to the second number*/
+		sum = fib1 + fib2;
+
+		printf("%d", sum);
+
+		/*set values of variables for the next count*/
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+			printf("\n");
+
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
