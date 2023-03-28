6263bb597d5ec00e92bcd7de98ca3a166fa06fdf
deantosh <deantoshdaiddoh@gmail.com>
2023-03-28 14:36:24 -0700

    task 72

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * puts_half -  prints half of a string, if number of characters is odd
+ *		print the last n characters of the string.
+ *
+ * @str: string to modify
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	/*declare variables*/
+	int i = 0;
+	int len = 0;
+	int m;
+
+	/*find length of str*/
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if(len % 2 == 0)
+	{
+		m = len / 2;
+	}
+	else
+	{
+		m = (len - 1) / 2;
+	}
+	
+	/*print half the characters*/
+	for (i = m; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
