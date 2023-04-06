dee400626640b53f559a7aba8b390284cac49d2a
deantosh <deantoshdaiddoh@gmail.com>
2023-04-06 02:01:42 -0700

    task 4: initial commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Gets the value of x raised to the power of y.
+ * @x: The number to compute its power.
+ * @y: The power of the number.
+ *
+ * Return: Value of x if y >= 0 and -1 if y < 0.
+ */
