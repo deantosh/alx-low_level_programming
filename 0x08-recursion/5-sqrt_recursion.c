680ba9a019bee20743f9fd8b4d353335ef67d796
deantosh <deantoshdaiddoh@gmail.com>
2023-04-06 03:27:19 -0700

    task 5: initial commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Computes the natural square root of a number.
+ * @n: The number to compute.
+ *
+ * Return: Natural square root of a number
+ *		-1 if number does not have a square root.
+ */
