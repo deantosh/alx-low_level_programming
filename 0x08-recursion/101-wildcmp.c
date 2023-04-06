e179841ff85f9e2b4fa16e048e7dfd27999e8c0d
deantosh <deantoshdaiddoh@gmail.com>
2023-04-06 11:54:01 -0700

    task 8: intial commit

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * wildcmp - Compares two strings.
+ * @s1: A pointer to the first string to be compared.
+ * @s2: A pointer to the second string to be compared.
+ *
+ * Return:  1 - if string is identical and 0 - if not identical.
+ */
