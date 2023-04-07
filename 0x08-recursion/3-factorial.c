a9dfbe7b1b08d9fb4478bced11b31dd54e87ad8f
deantosh <deantoshdaiddoh@gmail.com>
2023-04-06 23:08:13 -0700

    task 3: updated

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,5 +4,28 @@
  * factorial - Gets the factorial of a given number.
  * @n: The number to computed.
  *
- * Return: The factorial of the number.
+ * Return: The factorial of the number (n > 0).
+ *	   -1 if (n < 0).
+ *	   1 if (n == 0).
  */
+
+int factorial(int n)
+{
+	/*declare variable*/
+	int num = 1;
+
+	if (n > 0)
+	{
+		num *= n;
+		num *= factorial(n - 1);
+	}
+	else if (n < 0)
+	{
+		num = -1;
+	}
+	else
+	{
+		num = 1;
+	}
+	return (num);
+}
