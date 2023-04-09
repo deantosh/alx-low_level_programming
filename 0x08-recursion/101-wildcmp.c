0d282e86366a7b102ffa4fe66b62a539b607833a
deantosh <deantoshdaiddoh@gmail.com>
2023-04-09 00:18:33 -0700

    task 9: updated

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,3 +7,22 @@
  *
  * Return:  1 - if string is identical and 0 - if not identical.
  */
+
+int wildcmp(char *s1, char *s2)
+{
+	int result;
+
+	if (*s1 == *s2)
+		result = 1;
+
+	if (*s1 != *s2 && *s2 == '*')
+		result = 1;
+
+	if (*s1 != *s2)
+		result = 0;
+
+	if (*s1 == '\0')
+		return (result);
+
+	return (wildcmp(s1 + 1, s2 + 1));
+}
