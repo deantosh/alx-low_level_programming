2a93844559771d5dffcc3d6a0ffe0630d2faeb85
deantosh <deantoshdaiddoh@gmail.com>
2023-04-10 12:08:51 -0700

    task 0: unused variable warning fixed

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,15 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
  * main - Entry point.
  * @argc: The number of arguments passed.
- * @argv: A pointer to strings passed as arguments.
+ * @argv:  An array of pointers to the arguments.
  *
  * Return: Always (0) Success.
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	(void)argc;
 	return (0);
 }
