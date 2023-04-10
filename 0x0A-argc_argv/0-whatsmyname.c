ab745acc7928b5621737720db61643232813e9bc
deantosh <deantoshdaiddoh@gmail.com>
2023-04-10 11:58:57 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ * @argc: The number of arguments passed.
+ * @argv: A pointer to strings passed as arguments.
+ *
+ * Return: Always (0) Success.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
