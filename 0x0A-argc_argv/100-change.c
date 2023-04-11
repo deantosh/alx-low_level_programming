327ef710c4a558b0e19f698b095351d49599ba6a
deantosh <deantoshdaiddoh@gmail.com>
2023-04-11 00:30:48 -0700

    Additional task: identation error fixed

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -16,38 +16,40 @@ int main(int argc, char **argv)
 	int cents, coins = 0;
 
 	if (argc != 2)
+	{
 		printf("Error\n");
 		return (1);
-
+	}
 	if (atoi(argv[1]) < 0)
+	{
 		printf("%d\n", 0);
 		return (0);
-
+	}
 	cents = atoi(argv[1]);
-
 	while (cents > 0)
 	{
 		coins++;
-
 		if (cents > 24)
+		{
 			cents -= 25;
 			continue;
-
+		}
 		if (cents > 9)
+		{
 			cents -= 10;
 			continue;
-
+		}
 		if (cents > 4)
+		{
 			cents -= 5;
 			continue;
-
+		}
 		if (cents > 1)
+		{
 			cents -= 2;
 			continue;
-
-		if (cents > 0)
-			cents -= 1;
-			continue;
+		}
+		cents--;
 	}
 	printf("%d\n", coins);
 	return (0);
