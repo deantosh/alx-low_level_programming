1e5442eac51d0643cd28d33ed4ae26503fbd23f4
deantosh <deantoshdaiddoh@gmail.com>
2023-04-11 00:14:35 -0700

    Betty style error on number of line above 40 in a function: fixed

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point.
+ * main - Prints the minimum number of coins
+ *	  to make change for an amount of money.
+ *
  * @argc: The number of arguments passed.
  * @argv:  An array of pointers to the arguments.
  *
@@ -11,22 +13,16 @@
 
 int main(int argc, char **argv)
 {
-	/*declare variable*/
 	int cents, coins = 0;
 
 	if (argc != 2)
-	{
 		printf("Error\n");
 		return (1);
-	}
 
 	if (atoi(argv[1]) < 0)
-	{
 		printf("%d\n", 0);
 		return (0);
-	}
 
-	/*convert to int*/
 	cents = atoi(argv[1]);
 
 	while (cents > 0)
@@ -34,30 +30,24 @@ int main(int argc, char **argv)
 		coins++;
 
 		if (cents > 24)
-		{
 			cents -= 25;
 			continue;
-		}
+
 		if (cents > 9)
-		{
 			cents -= 10;
 			continue;
-		}
+
 		if (cents > 4)
-		{
 			cents -= 5;
 			continue;
-		}
+
 		if (cents > 1)
-		{
 			cents -= 2;
 			continue;
-		}
+
 		if (cents > 0)
-		{
 			cents -= 1;
 			continue;
-		}
 	}
 	printf("%d\n", coins);
 	return (0);
