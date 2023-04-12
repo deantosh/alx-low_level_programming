6c4b8d7aee8f19f032b5f755145371e117b69916
deantosh <deantoshdaiddoh@gmail.com>
2023-04-12 13:07:08 -0700

    task 0: output error message added

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,12 +16,16 @@ char *create_array(unsigned int size, char c)
 	char *str;
 	unsigned int i;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
 	str = malloc((size) * sizeof(char));
 	if (str == NULL)
 	{
-		printf("failed to allocate memory");
 		return (NULL);
 	}
+
 	i = 0;
 	while (i < size)
 	{
