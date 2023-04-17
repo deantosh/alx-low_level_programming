f161d73f0083f17801a79a66aff2d18c16e59f0a
deantosh <deantoshdaiddoh@gmail.com>
2023-04-17 09:04:16 -0700

    task 2: upated

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -28,7 +28,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (mem == NULL)
 		return (NULL);
 
-	for (index = 0; index < nmemb; index++)
+	for (index = 0; index < byte_size; index++)
 		mem[index] = 0;
 
 	return (mem);
