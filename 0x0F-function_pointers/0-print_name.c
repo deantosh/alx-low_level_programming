75a8eee2655ee811e57421cebf6e23c516583b07
deantosh <deantoshdaiddoh@gmail.com>
2023-04-19 11:23:34 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+/*
+ * File: 0-print_name.c
+ * Author: Deantosh M Daiddoh
+ */
+
+#include "function_pointers.h"
+
+/**
+ * print_name  Prints a name.
+ * @name: The name to print.
+ * @f: A pointer to a function.
+ *
+ * Return: void.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
