ae7043c1eab2872cd20bbe9ee539376dd066d311
deantosh <deantoshdaiddoh@gmail.com>
2023-04-22 00:48:00 -0700

    task 1: error fixed

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -25,7 +25,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(ap, int));
 
 		if (separator != NULL && index != (n - 1))
-			printf("%s ", separator);
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(ap);
