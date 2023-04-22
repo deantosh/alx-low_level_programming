67d9eb5b082803881117d515de6fb2d91acbd72f
deantosh <deantoshdaiddoh@gmail.com>
2023-04-22 00:34:55 -0700

    task 1: output results error fixed

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -24,7 +24,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(ap, int));
 
-		if (separator != NULL || index != (n - 1))
+		if (separator != NULL && index != (n - 1))
 			printf("%s", separator);
 
 		if (index != (n - 1))
