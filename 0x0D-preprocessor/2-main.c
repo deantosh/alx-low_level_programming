9ca3c2ae6c804fe0243824ab4345fc3489a29225
deantosh <deantoshdaiddoh@gmail.com>
2023-04-22 07:50:15 -0700

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,22 @@
+/*
+ * File: 2-main.c
+ * Author: Deantosh M Daiddoh
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	char *file_name;
+
+	file_name = __FILE__;
+	printf("%s\n", file_name);
+
+	return (0);
+}
