c2607ddcf8fdbdf47919e4cf127ca96450b04152
deantosh <deantoshdaiddoh@gmail.com>
2023-04-27 14:56:43 -0700

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,36 @@
+/*
+ * File: 0-print_list.c
+ * Author: Deantosh M Daiddoh
+ */
+
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_list - Prints the elements of a list.
+ * @h: A pointer to the list.
+ *
+ * Return: The number of elements in the list.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] ");
+			printf("(nil)\n");
+		}
+		else
+		{
+			printf("[%d] ", h->len);
+			printf("%s\n", h->str);
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
