7640ab53f699fc4ed1bebc9858e7e55724b9a0e3
deantosh <deantoshdaiddoh@gmail.com>
2023-05-01 11:58:21 -0700

    task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,110 @@
+/*
+ * File: 3-print_all.c
+ * Author: Deantosh M Daiddoh
+ */
+
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_all - Prints anything.
+ * @format:  A pointer to the list of types of the arguments.
+ *
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i = 0, j;
+	va_list args;
+	char *separator = "";
+	f_s s_list[] = {
+		{"c", print_a_char},
+		{"i", print_an_integer},
+		{"f", print_a_float},
+		{"s", print_a_string}
+	};
+
+	va_start(args, format);
+	while (format != NULL && format[i])
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == s_list[j].sym[0])
+			{
+				s_list[j].f(separator, args);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}
+
+/**
+ * print_a_char - Prints a character.
+ * @separator:  A pointer to the separator used to separate char
+ *		from other arguments when printing.
+ * @args: The list  of arguments to print.
+ *
+ * Return: void
+ */
+void print_a_char(char *separator, va_list args)
+{
+	char ch = va_arg(args, int);
+
+	printf("%s%c", separator, ch);
+}
+
+/**
+ * print_an_integer - Prints an integer.
+ * @separator:  A pointer to the separator used to separate char
+ *		from other arguments when printing.
+ * @args: The list  of arguments to print.
+ *
+ * Return: void
+ */
+void print_an_integer(char *separator, va_list args)
+{
+	int num = va_arg(args, int);
+
+	printf("%s%d", separator, num);
+}
+
+/**
+ * print_a_float - Prints a floating number.
+ * @separator:  A pointer to the separator used to separate char
+ *		from other arguments when printing.
+ * @args: The list  of arguments to print.
+ *
+ * Return: void
+ */
+void print_a_float(char *separator, va_list args)
+{
+	double num = va_arg(args, double);
+
+	printf("%s%f", separator, num);
+}
+
+/**
+ * print_a_string - Prints a string.
+ * @separator:  A pointer to the separator used to separate char
+ *		from other arguments when printing.
+ * @args: The list  of arguments to print.
+ *
+ * Return: void
+ */
+void print_a_string(char *separator, va_list args)
+{
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+	{
+		printf("%s%s", separator, "(nill)");
+	}
+
+	printf("%s%s", separator, str);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,31 @@
 #ifndef VAR_H
 #define VAR_H
 
+#include <stdarg.h>
+
+/**
+ * struct format_list - Defines the data structure of a format specifier.
+ * @sym: A pointer to the specified symbol.
+ * @f: A function pointer to the equivalent symbol function.
+ *
+ * Description: Defines the symbol data type and its equivalent
+ *		function to print the argument of that type.
+ */
+typedef struct format_list
+{
+	char *sym;
+	void (*f)(char *separator, va_list args);
+} f_s;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/*HELPER FUNCTIONS - print_all*/
+void print_a_char(char *separator, va_list args);
+void print_an_integer(char *separator, va_list args);
+void print_a_float(char *separator, va_list args);
+void print_a_string(char *separator, va_list args);
 
 #endif /*VAR_H*/
