51b4dabaf38bb397fbf2038aed84d6df76374b00
deantosh <deantoshdaiddoh@gmail.com>
2023-05-03 11:47:44 -0700

    task 6: output result - (head == NULL)error fixed

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -17,6 +17,9 @@ int pop_listint(listint_t **head)
 	listint_t *delete_node;
 	int node_data;
 
+	if (*head == NULL)
+		return (0);
+
 	delete_node = (*head);
 	*head = (*head)->next; /*move head to next node*/
 	node_data = delete_node->n;
