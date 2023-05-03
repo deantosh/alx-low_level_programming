0e4023056ce47d10f907d9218cafda70164005b4
deantosh <deantoshdaiddoh@gmail.com>
2023-05-03 12:58:51 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	/*get the the prev_node*/
 	prev_node = *head;
-	for (index = 0; index < (idx - 1); index++)
+	for (index = 0; index < idx ; index++)
 	{
 		if (prev_node->next == NULL)
 			return (NULL);
