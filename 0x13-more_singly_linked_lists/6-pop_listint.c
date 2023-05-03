7515dc4fc8fa469aa4d96944249512acf833541d
deantosh <deantoshdaiddoh@gmail.com>
2023-05-03 11:37:59 -0700

    task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+/*
+ * File: 6-pop_listint.c
+ * Author: Deantosh M Daiddoh
+ */
+
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - Deletes the head node of the linked list.
+ * @head: A pointer to the list.
+ *
+ * Return: The head node data.
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *delete_node;
+	int node_data;
+
+	delete_node = (*head);
+	*head = (*head)->next; /*move head to next node*/
+	node_data = delete_node->n;
+	free(delete_node);
+
+	return (node_data);
+}
