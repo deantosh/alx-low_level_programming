5893004c9886c99bed2de319eaa4abe32e7ec3bd
deantosh <deantoshdaiddoh@gmail.com>
2023-05-06 04:24:47 -0700

    get_bit function prototype added

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,5 +6,6 @@ unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_length(const char *str);
 void get_bits(unsigned long int num);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif /*MAIN_H*/
