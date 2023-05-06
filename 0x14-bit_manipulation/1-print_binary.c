e61cd3a248e2415e3bbfe633c1db3bbb11d92a25
deantosh <deantoshdaiddoh@gmail.com>
2023-05-06 04:08:31 -0700

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,49 @@
+/*
+ * File: 1-print_binary.c
+ * Author: Deantosh M Daiddoh
+ */
+
+#include "main.h"
+
+/**
+ * print_binary - Converts integer to binary
+ * @n: The number to convert.
+ *
+ * Return: void.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	get_bits(n);
+}
+
+/**
+ * get_bits - Gets the bits equivalent of the number.
+ * @num: The number to convert.
+ *
+ * Return: void.
+ */
+void get_bits(unsigned long int num)
+{
+	int bit_value;
+
+	if (num < 1)
+		return;
+
+	get_bits(num >> 1); /*divides the num by 2*/
+
+	bit_value = num & 1; /*gets the bit value 0 or 1*/
+	if (bit_value)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
