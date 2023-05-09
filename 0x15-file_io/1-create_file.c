32adc27f2333117f80431f49051547ab955af75d
deantosh <deantoshdaiddoh@gmail.com>
2023-05-09 10:55:58 -0700

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,53 @@
+/*
+ * File: 1-create_file.c
+ * Author: Deantosh M Daiddoh
+ */
+
+#include "main.h"
+
+/**
+ * create_file - Creates a file.
+ * @filename: The name of the file to create.
+ * @text_content: The content to write to  file.
+ *
+ * Return: 1 (Success) and -1 fails.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, cnt;
+
+	/*get length of the string*/
+	cnt = str_len(text_content);
+
+	if (filename == NULL)
+		return (-1);
+
+	/*create a file*/
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 400);
+	if (fd == -1)
+		return (-1);
+
+	/*write into the file*/
+	write(fd, text_content, cnt);
+
+	/*close file*/
+	close(fd);
+
+	return (1);
+}
+
+/**
+ * str_len - Gets the string length.
+ * @str: A pointer to the string.
+ *
+ * Return: The length.
+ */
+int str_len(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		i++;
+
+	return (i);
+}
