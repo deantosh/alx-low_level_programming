7570cfc8ffc0ce202eb93c913131d3aeb64921f5
deantosh <deantoshdaiddoh@gmail.com>
2023-05-09 11:03:51 -0700

    task 1: file permission changed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	/*create a file*/
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 400);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
