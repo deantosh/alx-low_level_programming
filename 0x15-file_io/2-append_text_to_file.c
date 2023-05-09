bb8f6721cc2c258e482a51cfb1090664e23c2539
deantosh <deantoshdaiddoh@gmail.com>
2023-05-09 14:29:29 -0700

    task 2: output error if content to append is null, fixed

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -30,6 +30,13 @@ int append_text_to_file(const char *filename, char *text_content)
 
 		write(fd, text_content, cnt);
 	}
+	else
+	{
+		if (filename)
+			return (1);
+		else
+			return (-1);
+	}
 
 	return (1);
 }
