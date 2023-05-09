c16aa851b43124425a3c9545088ef3c6c9603d2e
deantosh <deantoshdaiddoh@gmail.com>
2023-05-09 14:18:29 -0700

    task 2: updated

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,12 +22,14 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_APPEND,  S_IRUSR | S_IWUSR);
-	if (fd == -1)
-		return (-1);
-
 	if (text_content)
+	{
+		fd = open(filename, O_RDWR | O_APPEND,  S_IRUSR | S_IWUSR);
+		if (fd == -1)
+			return (-1);
+
 		write(fd, text_content, cnt);
+	}
 
 	return (1);
 }
