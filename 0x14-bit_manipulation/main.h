d56927355cbdf21d14be44d9c5d00d299c1e5b36
deantosh <deantoshdaiddoh@gmail.com>
2023-07-06 09:48:39 -0700

    function prototype added

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*task 0*/
 unsigned int binary_to_uint(const char *b);
 int get_length(const char *str);
+
+/*task 1*/
+void print_binary(unsigned long int n);
+void get_bits(unsigned long int num);
+int _putchar(char c);
 
 #endif /*MAIN_H*/
