0c5ec30c958c73467fc7babde83bcf84a868f1f3
deantosh <deantoshdaiddoh@gmail.com>
2023-07-06 04:46:37 -0700

    task 0: binary to uint

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+/**
+ * binary_to_uint - Converts from binary to unsigned integer.
+ * @b: A pointer to the bit string.
+ *
+ * Return: The unsigned integer (success) & 0 (fails).
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int sum = 0, count = 0, len = 0;
+
+	if (b == NULL)
+		return (0);
+
+	/*get length of string*/
+	len = get_length(b);
+
+	while (len >= 0)
+	{
+		if (b[len] < 48 || b[len] > 49)
+			return (0);
+
+		if (b[len] == 49)
+		{
+			sum += 1 << count;
+		}
+		count++;
+		len--;
+	}
+	return (sum);
+}
+
+/**
+ * get_length - Gets the length of the binary array.
+ * @str: A pointer to the binary number.
+ *
+ * Return: The length of the array.
+ */
+int get_length(char *str)
+{
+	int index = 0, len = 0;
+
+	while (str[index] != '\0')
+	{
+		len++;
+		index++;
+	}
+	return (len);
+}
