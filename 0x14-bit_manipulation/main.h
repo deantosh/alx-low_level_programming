968af2e868e7767a365b33a73646fb2f08dc2b05
deantosh <deantoshdaiddoh@gmail.com>
2023-07-06 10:45:23 -0700

    clear_bit function prototype added

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -16,4 +16,7 @@ int get_bit(unsigned long int n, unsigned int index);
 /*task 3*/
 int set_bit(unsigned long int *n, unsigned int index);
 
+/*task 4*/
+int clear_bit(unsigned long int *n, unsigned int index);
+
 #endif /*MAIN_H*/
