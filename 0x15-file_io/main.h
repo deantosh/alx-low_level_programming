ff0ef721f19fc761102a6d2aa6f13f25b6b865e0
deantosh <deantoshdaiddoh@gmail.com>
2023-07-10 08:13:23 -0700

    create_file function prototype added

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*include files*/
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -10,4 +11,6 @@
 /*task 0*/
 ssize_t read_textfile(const char *filename, size_t letters);
 
+/*task 1*/
+int create_file(const char *filename, char *text_content);
 #endif /*MAIN_H*/
