983aabce6b17d938e7ddf83832900ce29d833215
deantosh <deantoshdaiddoh@gmail.com>
2023-07-10 11:42:04 -0700

    function prototype added

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -17,7 +17,12 @@ int create_file(const char *filename, char *text_content);
 /*task 2*/
 int append_text_to_file(const char *filename, char *text_content);
 
-/*common*/
-int str_len(char *str);
+/*task 3*/
+int main(int argc, char **argv);
+void copy_file(char *src_file, char *dest_file);
+
+/*task 4*/
+int main(int argc, char **argv);
+void copy_file(char *src_file, char *dest_file);
 
 #endif /*MAIN_H*/
