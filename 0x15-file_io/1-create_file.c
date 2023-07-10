22f760f1e92f3028535428b7fbfb441e8a6e5d4f
deantosh <deantoshdaiddoh@gmail.com>
2023-07-10 11:08:00 -0700

    task 0: file updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,3 +30,19 @@ int create_file(const char *filename, char *text_content)
 
 	return (1);
 }
+
+/**
+ * str_len - Gets the string length.
+ * @str: A pointer to the string.
+ *
+ * Return: The length.
+ */
+int str_len(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		i++;
+
+	return (i);
+}
