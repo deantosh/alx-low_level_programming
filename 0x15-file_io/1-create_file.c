c0631d6ba368aad5db68baf81a62a5ee19e4d895
deantosh <deantoshdaiddoh@gmail.com>
2023-07-11 00:39:40 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,23 +12,17 @@ int create_file(const char *filename, char *text_content)
 	int fd, fw, cnt;
 
 	/*get length of the string*/
-	cnt = str_len(text_content);
+	if (text_content != NULL)
+		cnt = str_len(text_content);
 
 	if (filename == NULL)
 		return (-1);
 
-	/*create a file*/
+	/*create a file and write into it*/
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fd == -1)
+	fw = write(fd, text_content, cnt);
+	if (fd == -1 || fw == -1)
 		return (-1);
-
-	/*write into the file*/
-	if (text_content != NULL)
-	{
-		fw = write(fd, text_content, cnt);
-		if (fw == -1)
-			return (-1);
-	}
 
 	/*close file*/
 	close(fd);
