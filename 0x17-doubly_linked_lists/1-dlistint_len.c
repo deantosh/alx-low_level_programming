467576860409f1c6dcdb85c827c3f5663b41a0a9
deantosh <deantoshdaiddoh@gmail.com>
2023-08-17 04:22:24 -0700

    task 1: list length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,25 @@
+/*
+ * Author: Deantosh Daiddoh
+ * File: 0-print_dlistint.c
+ */
+
+#include "lists.h"
+
+/**
+ * dlistint_len - Gets the length of a doubly linked list.
+ * @h: A pointer to the list.
+ *
+ * Return: The number of nodes in list.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+	size_t num_nodes = 0;
+
+	while (current)
+	{
+		num_nodes++;
+		current = current->next; /*move ptr*/
+	}
+	return (num_nodes);
+}
