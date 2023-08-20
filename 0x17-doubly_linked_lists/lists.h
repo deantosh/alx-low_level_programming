d89fbfc0575b94e4d9581f262b4452815f1d3efa
deantosh <deantoshdaiddoh@gmail.com>
2023-08-19 23:33:31 -0700

    file: updated

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -29,5 +29,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 dlistint_t *add_node_nth_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+void search_del_node(dlistint_t **head, unsigned int index, unsigned int len);
 
 #endif /*LISTS_H*/
