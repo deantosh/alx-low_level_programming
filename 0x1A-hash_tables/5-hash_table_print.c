a99635edc3615415b91fd206d031ccc9995b2616
deantosh <deantoshdaiddoh@gmail.com>
2023-11-06 01:02:47 -0800

    task 5: solution added

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,49 @@
+/*
+ * Author: Deantosh M Daiddoh
+ * File: 5-hash_table_print.c
+ */
+
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table.
+ * @ht: The hash table.
+ *
+ * Return: void.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *curr;
+	unsigned long int i;
+	int set = 0;
+
+	if (ht == NULL)
+		printf("{}");
+	else
+	{
+		printf("{");
+
+		for (i = 0; i < ht->size; i++)
+		{
+			curr = ht->array[i];
+			if (curr)
+			{
+				if (set > 0)
+					printf(", ");
+				if (curr->next)
+				{
+					while (curr)
+					{
+						printf("'%s': '%s'", curr->key, curr->value);
+						curr = curr->next;
+					}
+				}
+				else
+					printf("'%s': '%s'", curr->key, curr->value);
+				set++;
+			}
+		}
+		printf("}");
+	}
+	printf("\n");
+}
