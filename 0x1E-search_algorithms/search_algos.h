c6772f44c0e7e16efdc2ca45b9acb305430d5d94
root <deantoshdaiddoh@gmail.com>
2024-02-06 11:58:48 -0800

    binary search function prototype added

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,8 +3,11 @@
 
 /*Headers*/
 #include <stdio.h>
+#include <math.h>
 
 /*Function prototypes*/
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int find_value(int *array, int low, int high, int value);
 
 #endif /*SEARCH_H*/
