150497a6c7208f333680be39ab68956ca58cb494
root <deantoshdaiddoh@gmail.com>
2024-02-07 09:55:23 -0800

    interpolation search function prototype added

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,11 +4,14 @@
 /*Headers*/
 #include <stdio.h>
 #include <math.h>
+#include <stddef.h>
 
 /*Function prototypes*/
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int find_value(int *array, int low, int high, int value);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int interpolate_value(int *array, int low, int high, int value);
 
 #endif /*SEARCH_H*/
