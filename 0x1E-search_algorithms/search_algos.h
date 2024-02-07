acec99e5e7cfa1c1f48c9fa88479c45b36194a82
root <deantoshdaiddoh@gmail.com>
2024-02-07 04:34:06 -0800

    jump search function added

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,5 +9,6 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int find_value(int *array, int low, int high, int value);
+int jump_search(int *array, size_t size, int value);
 
 #endif /*SEARCH_H*/
