3fdc45ad9a4525ec6916f538ae2e5ddf03b83a37
deantosh <deantoshdaiddoh@gmail.com>
2024-07-17 07:39:16 -0700

    exponential search algo function prototypes

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -13,5 +13,9 @@ int find_value(int *array, int low, int high, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int interpolate_value(int *array, int low, int high, int value);
+int exponential_search(int *array, size_t size, int value);
+
+/*Helper functions*/
+void print_subarray(int *array, int low, int high);
 
 #endif /*SEARCH_ALGORITHMS_H*/
