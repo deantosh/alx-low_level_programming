bd7dbf718e099718646cba0fd8ce99517551af32
deantosh <deantoshdaiddoh@gmail.com>
2024-07-18 05:49:16 -0700

    function prototype added

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -14,8 +14,10 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int interpolate_value(int *array, int low, int high, int value);
 int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 /*Helper functions*/
 void print_subarray(int *array, int low, int high);
+int advanced_binary_search(int *array, int low, int high, int value);
 
 #endif /*SEARCH_ALGORITHMS_H*/
