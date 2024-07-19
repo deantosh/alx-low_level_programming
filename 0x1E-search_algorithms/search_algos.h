8cbfef87bcd20a8721a0a84aa4211e81649c4a77
deantosh <deantoshdaiddoh@gmail.com>
2024-07-19 10:09:07 -0700

    function prototype added

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,6 +6,22 @@
 #include <math.h>
 #include <stddef.h>
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
 /*Function prototypes*/
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
@@ -15,9 +31,11 @@ int interpolation_search(int *array, size_t size, int value);
 int interpolate_value(int *array, int low, int high, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 
 /*Helper functions*/
 void print_subarray(int *array, int low, int high);
 int advanced_binary_search(int *array, int low, int high, int value);
+listint_t *move_ptr(listint_t *curr, size_t size);
 
 #endif /*SEARCH_ALGORITHMS_H*/
